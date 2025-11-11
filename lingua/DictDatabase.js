55a2cdb5d9d6de75ef2e54be3af7be3ac449f343
Shiroko4592 <runarunayeh@naver.com>
2025-11-11 18:15:29 +0800

    Create DictDatabase.js

---
+++ lingua/DictDatabase.js
@@ -0,0 +1,8 @@
+// 사전 데이터베이스 초기화
+const DictDatabase = [
+    { word: "犬", meaning: "개" },
+    { word: "猫", meaning: "고양이" },
+    { word: "学校", meaning: "학교" },
+    { word: "本", meaning: "책" },
+    { word: "水", meaning: "물" }
+];
